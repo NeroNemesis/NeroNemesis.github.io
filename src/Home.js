1fe48c2d505e55ebe7923d29b96aa5be772c0108
NeroNemesis <72110273+NeroNemesis@users.noreply.github.com>
2023-06-27 11:32:24 -0400

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const Home = () =>
 {
     const [projects, setProjects] = useState([
-        { id: 1, title: 'HandyMan', description: 'lorem ipsum dolor...', viewLink: '#', repoLink: '#', imageURL: process.env.PUBLIC_URL + '/images/handyMan.jpg'},
+        { id: 1, title: 'HandyMan', description: 'lorem ipsum dolor...', viewLink: 'https://neronemesis.github.io/handyMan/', repoLink: 'https://github.com/NeroNemesis/handyMan', imageURL: process.env.PUBLIC_URL + '/images/handyMan.jpg'},
         { id: 2, title: 'CarPoolCITY', description: 'lorem ipsum dolor...', viewLink: '#', repoLink: '#', imageURL: '#'}
     ]);
 
